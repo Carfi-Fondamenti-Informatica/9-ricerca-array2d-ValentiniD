8966ce7e9afae4b28e5936ffc4aeeecb80eba360
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-10 16:54:03 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,34 @@
+//
+// Created by gasta on 10/05/2022.
+//
 
+#include "lib.h"
+
+char init(char mat[][20], int dim){
+    for(int i=0; i<dim; i++){
+        for(int j=0; j<20; i++){
+            mat[i][j]=0;
+        }
+    }
+}
+char init(char ar[20]){
+    for(int i=0; i<20; i++){
+        ar[i]=0;
+    }
+}
+
+int ricerca_nomi(char elenco_nomi[10][20], char inserimento_nomi[20]){
+    int a=0;
+    for(int i=0; i<10; i++){
+        a=0;
+        for(int j=0; j<20; j++){
+            if(elenco_nomi[i][j]==inserimento_nomi[j]){
+                a++;
+            }else if(a==20){
+                return i;
+            }else{
+                break;
+            }
+        }return -1;
+    }
+}
