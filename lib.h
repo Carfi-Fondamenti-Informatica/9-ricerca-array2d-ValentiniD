8c1aa3e3e64892ef6a4748455d845a34d83dcb94
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-10 16:54:41 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,13 @@
+//
+// Created by gasta on 10/05/2022.
+//
 
+#ifndef RICERCA_ARRAY_2D_LIB_H
+#define RICERCA_ARRAY_2D_LIB_H
+
+#endif //RICERCA_ARRAY_2D_LIB_H
+
+char init(char mat[][20], int dim);
+char init(char ar[20]);
+
+int ricerca_nomi(char elenco_nomi[10][20], char inserimento_nomi[20]);
