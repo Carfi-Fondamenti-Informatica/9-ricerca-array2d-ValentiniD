e7ca0f7eba5d9e13a4782e4fbc23e6c6cbcd6031
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-10 16:53:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char elenco_nomi[10][20];
+    char inserimento_nomi[20];
+    init(elenco_nomi, 10);
+    init(inserimento_nomi);
+    int a;
+
+    for(int i=0; i<20; i++){
+        init(inserimento_nomi);
+        cin >> inserimento_nomi;
+        for(int j=0; j<10; j++){
+            elenco_nomi[j][i] = inserimento_nomi[j];
+        }
+    }
+    a = ricerca_nomi(elenco_nomi, inserimento_nomi);
+    if (a != -1) {
+        cout << a;
+    } else {
+        cout << "non presente" << endl;
+    }
+    return 0;
 }
